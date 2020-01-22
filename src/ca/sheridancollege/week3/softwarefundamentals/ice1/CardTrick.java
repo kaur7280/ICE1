a25fa3eece771b4f9ead172b0b9edc8f422be1ae
ramne <ramne@DESKTOP-GS1EOHH>
2020-01-21 19:44:53 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,38 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+                c.setValue(1 + (int)Math.random()*13);
+ c.setSuit(Card.SUITS[1+((int)Math.random()*4)]);
+            magicHand[i]=c;
+        
+           }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+           //
+           
+        Scanner k = new Scanner(System.in);
+        
+        System.out.print("Enter Card Value: ");
+        int value = k.nextInt();
+        
+        System.out.print("Enter Suit: ");
+        String suit=k.next();
+        
+        for(int sub=0; sub<magicHand.length; sub++)
+        {
+            if(value == magicHand[sub].getValue())
+            {
+                if(suit.equalsIgnoreCase(magicHand[sub].getSuit()))
+                {
+                    System.out.println("Card Found");
+                }
+            }
+            else
+                System.out.println("Card Not Found");
+            
+        
     }
-    
+    }
 }
